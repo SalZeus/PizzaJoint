e526a5a63cf9737af63f0a5408cecb2155cec6c1
Juan Felipe Gutiérrez <juanfeguto2@gmail.com>
2023-10-21 17:31:32 -0500

    node modules update

--- pizzajoint/src/App.js
+++ pizzajoint/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/Home';
 import Base from './components/Base';
